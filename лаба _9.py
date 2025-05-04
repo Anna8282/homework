72f10b216d4342d94ce12c3f130cbdd181b7f1b3
Anna8282 <pavlenkoanna@knu.ua>
2025-05-04 13:30:46 +0300

    def add

---
+++ лаба _9.py
@@ -0,0 +1,31 @@
+from math import gcd
+
+
+class Rational:
+    def __init__(self, n, d):
+        self._n = n
+        self._d = d
+
+    def __str__(self):
+        return str(self._n) + '/' + str(self._d)
+
+    def __call__(self):  # круглі дужки
+        return self._n / self._d
+
+    def __add__(self, other):
+        result = Rational(0, 1)
+        if isinstance(other, int):
+            other = Rational(other, 1)
+        elif not isinstance(other, Rational):
+            raise TypeError
+
+        zn = self._d * other.d // gcd(self._d, other.d)
+        num = self._n * (zn // self._d) + other * (zn // other.d)
+
+        return Rational(num, zn)
+
+        # визначити знаменник нсд, зведення до нескоротного дробу
+
+
+num = Rational(1, 2)
+print(num())
